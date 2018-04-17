e5dab756b762c8484262305f62eb71f53bc6a136
Asami Tachibana <atachibana2612@gmail.com>
2018-04-17 18:12:56 -0400

    eh

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Action816
-Trying to emulate a custom 65816-based personal computer.
+Trying to emulate a custom 65816-based personal computer using non-proprietary hardware design with the intent of running a version of Microsoft BASIC.
---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+
+  Action816 - A custom 65816 emulator
+  (c) 2018, James Maloy and Jeremy Brown, all rights reserved
+  Licensed under the MIT license.
+
+  file: main.c
+  desc: "Base for which we invent the universe."
+  date: 13 April 2018
+
+  **/
+
+
