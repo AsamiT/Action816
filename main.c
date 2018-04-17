2256ff174548bb707fec3d27d4b2077028f4286d
Asami Tachibana <atachibana2612@gmail.com>
2018-04-17 19:08:36 -0400

    Fiddly as ever.

--- main.c
+++ main.c
@@ -14,28 +14,32 @@
   #include <stdio.h>
   #include <stdint.h>
 
+  typedef struct sys_Flags { /* note: this is not finished */
+        uint8_t carry; //Carry flag
+        uint8_t zflag; //Zero flag
+        uint8_t irp_dis; //Interrupt Disable
+        uint8_t dec_mode; //Decimal mode
+        uint8_t brkcmd; //Break Command
+        uint8_t overflow; //Overflow flag
+        uint8_t negflag; //Negative flag
+        } sys_Flags;
+
   typedef struct State65816 {
-        uint8_t pc;
-        uint8_t sp;
-        uint8_t acc;
-        uint8_t irx;
-        uint8_t iry;
-        /** uint8_t carry;
-        uint8_t zflag;
-        uint8_t irp_dis;
-        uint8_t dec_mode;
-        uint8_t brkcmd;
-        uint8_t overflow;
-        uint8_t negflag; **/
-        // These are flags set by the processor. Need to review to make sure these are declared right.
-        uint8_t *memory;
+        uint8_t pc; //Program Counter
+        uint8_t sp; // Stack Pointer
+        uint8_t acc; //Accumulator
+        uint8_t irx; //Index Register X
+        uint8_t iry; //Index Register Y
+        uint8_t *memory; //Memory allocation
+
     } State65816;
 
     State65816* Init6502(void)
     {
+        size_t mem_size = 0x10000;
         State65816* state = calloc(1, sizeof(State65816));
-        state->memory = malloc(1*sizeof(uint8_t)); //this should have 64 kilobytes allocated
-        printf("%d bytes allocated.\n", sizeof(state->memory));
+        state->memory = malloc(mem_size); //this should have 64 kilobytes allocated
+        printf("%d bytes allocated.\n", _msize(state->memory)); //and it does
         return state;
     }
 
