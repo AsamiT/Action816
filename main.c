7651c283da4ed3c6cf1ecfe20301f8d598775354
Asami Tachibana <atachibana2612@gmail.com>
2018-04-17 20:47:41 -0400

    Removed SDL references because it does not work.

--- main.c
+++ main.c
@@ -13,17 +13,6 @@
   #include <stdlib.h>
   #include <stdio.h>
   #include <stdint.h>
-  #include "SDL2\SDL.h"
-
-  typedef struct sys_Flags { /* note: this is not finished */
-        uint8_t carry; //Carry flag
-        uint8_t zflag; //Zero flag
-        uint8_t irp_dis; //Interrupt Disable
-        uint8_t dec_mode; //Decimal mode
-        uint8_t brkcmd; //Break Command
-        uint8_t overflow; //Overflow flag
-        uint8_t negflag; //Negative flag
-        } sys_Flags;
 
   typedef struct State65816 {
         uint8_t pc; //Program Counter
@@ -31,13 +20,21 @@
         uint8_t acc; //Accumulator
         uint8_t irx; //Index Register X
         uint8_t iry; //Index Register Y
+        uint8_t sysFlags; //system flags
+        /*  bit 0: carry
+            bit 1: zero
+            bit 2: IRQ disable
+            bit 3: decimal mode
+            bit 4: break instruction
+            bit 5: unimplemented (do not use)
+            bit 6: overflow
+            bit 7: negative */
         uint8_t *memory; //Memory allocation
 
     } State65816;
 
     State65816* Init6502(void)
     {
-        gen_window();
         size_t mem_size = 0x10000;
         State65816* state = calloc(1, sizeof(State65816));
         state->memory = malloc(mem_size); //this should have 64 kilobytes allocated
